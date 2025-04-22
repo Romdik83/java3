9bb5b0942f6470687215bac186d2d87ca9ce892a
Romdik <romdik83@gmail.com>
2025-04-22 17:00:43 +0300

    change bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int balans = 100;
+        int bonus;
+        int newBalans;
+        int payment = 300;
+        if (payment > 1000) {
+            bonus = payment / 100;
+            newBalans = balans + bonus + payment;
+        } else {
+            bonus = 0;
+            newBalans = balans + payment;
+        }
+
+        System.out.println("Итоговая сумма: " + newBalans);
+
+
     }
 }
